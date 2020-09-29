cf13a2d7335f6704664975d87e535fbd5aef1174
JMV Hategekimana <Hategekamvianney@gmail.com>
2020-09-28 22:36:19 -0700

    this modification to my websie according last friday Demo

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
 const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
 const header = document.querySelector('.header.container');
-
+const nav_link = document.querySelectorAll('.nav-list ul li a');
 hamburger.addEventListener('click', ()=>{
     hamburger.classList.toggle('active');
     mobile_menu.classList.toggle('active');
@@ -14,4 +14,21 @@ document.addEventListener('scroll', ()=>{
     } else {
         header.style.backgroundColor = 'transparent';   
     }
+});
+
+window.addEventListener('scroll', event =>{
+    let fromTop = window.scrollY;
+
+   nav_link.forEach(link => {
+    let section = document.querySelector(link.hash);
+
+    if (section.offsetTop <= fromTop + 70 && section.offsetTop + section.offsetHeight > fromTop + 70){
+        link.style.color = 'crimson';
+        link.style.borderBottom = '2px solid crimson';
+        console.log(fromTop +30)
+    } else {
+        link.style.color = 'white';
+        link.style.borderBottom = 'none';
+    }
+   })
 });
