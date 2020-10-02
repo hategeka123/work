5f5a611e920936b2f3cb3f205d3561e84a860c7c
JMV Hategekimana <Hategekamvianney@gmail.com>
2020-10-01 21:36:36 -0700

    adding firebase and some modifiction on my website for all page

---
+++ firebase/firebase.js
@@ -0,0 +1,15 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAmgivkCT-OciVMUsAJ1-nMNPY8kqz7MFg",
+    authDomain: "andelaprojects.firebaseapp.com",
+    databaseURL: "https://andelaprojects.firebaseio.com",
+    projectId: "andelaprojects",
+    storageBucket: "andelaprojects.appspot.com",
+    messagingSenderId: "686063114377",
+    appId: "1:686063114377:web:f799ac5ab2c79f09ee0e58",
+    measurementId: "G-XZ0QETXC21"
+  };
+  firebase.initializeApp(firebaseConfig);
+  const db = firebase.firestore();
+  const auth = firebase.auth();
+  
