862a346276980d120a47aeeb7b45242cab4b63b9
andela <hategekimana Jmv>
2020-11-23 15:31:09 +0200

    changes

--- firebase/login.js
+++ firebase/login.js
@@ -1,4 +1,22 @@
 
+const inputs = document.querySelectorAll('.input');
+
+inputs.forEach(input => {
+    input.addEventListener('focus', focusFunction);
+    input.addEventListener('blur', blurFunction);
+});
+
+function focusFunction(){
+    let parent = this.parentNode.parentNode;
+    parent.classList.add('focus');
+}
+function blurFunction(){
+    let parent = this.parentNode.parentNode;
+    if(this.value == ""){
+        parent.classList.remove('focus');
+    }
+}
+
 const blogId = localStorage.getItem("blogId");
 const loginForm = document.querySelector("#login-form");
 loginForm.addEventListener('submit', (e) => {
