902f2d5b4c025dbfb7d3dca035842253bb2aa39a
andela <hategekimana Jmv>
2020-11-23 17:08:16 +0200

    authorize unloged in

--- firebase/reading.js
+++ firebase/reading.js
@@ -11,23 +11,44 @@ let month = dateObject.toLocaleString('en-GB',{month: 'short'});
 let year = dateObject.getFullYear()
 let day = dateObject.toLocaleDateString('en-GB', {day: '2-digit'})
 let dateResult = `${day}-${month}-${year}`
- 
-const blogDiv = `<div class="flexbox-blog-item" id=${doc.id}>
-                <img src="${doc.data().imageref}">
-                
-                <a href="/commet.html#/${doc.id}" onclick=readingBlogs("${doc.id}")><h3>${doc.data().title} </h3></a>
-                <span><strong>JMV</strong><span>${dateResult}</span></span> 
-                <p>
-                
-                ${doc.data().description.substring(0, 110)}...
-                </p>
-                
-            </div>`
+firebase.auth().onAuthStateChanged(user=>{
+    if(!user){
 
-            blog.innerHTML += blogDiv
-            const myId = document.querySelector('.flexbox-blog-item')
-            const iid = myId.getAttribute('id')
-            console.log(iid)
+        const blogDiv = `<div class="flexbox-blog-item" id=${doc.id}>
+                        <img src="${doc.data().imageref}">
+                        
+                        <a href="#" onclick=readingBlogs("${doc.id}")><h3>${doc.data().title} </h3></a>
+                        <span><strong>JMV</strong><span>${dateResult}</span></span> 
+                        <p>
+                        
+                        ${doc.data().description.substring(0, 110)}...
+                        </p>
+                        
+                    </div>`
+        
+                    blog.innerHTML += blogDiv
+                    const myId = document.querySelector('.flexbox-blog-item')
+                    const iid = myId.getAttribute('id')
+                    console.log(iid)
+    } else {
+        const blogDiv = `<div class="flexbox-blog-item" id=${doc.id}>
+                        <img src="${doc.data().imageref}">
+                        
+                        <a href="/commet.html#/${doc.id}" onclick=readingBlogs("${doc.id}")><h3>${doc.data().title} </h3></a>
+                        <span><strong>JMV</strong><span>${dateResult}</span></span> 
+                        <p>
+                        
+                        ${doc.data().description.substring(0, 110)}...
+                        </p>
+                        
+                    </div>`
+        
+                    blog.innerHTML += blogDiv
+                    const myId = document.querySelector('.flexbox-blog-item')
+                    const iid = myId.getAttribute('id')
+                    console.log(iid)
+    }
+})
 
             // <a ><h3 onclick="readingBlogs(${doc.id})">${doc.data().title}</h3></a>
     });
